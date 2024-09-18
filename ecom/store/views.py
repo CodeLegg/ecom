b32e8de17e4ab44b1bb4476652a65ce6ef7790c7
CodeLegg <michaelraymondlegg@gmail.com>
2024-09-18 18:10:03 +0100

    Refactor CollectionAdmin in store/admin.py

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -11,47 +11,55 @@ def wholesale(request):
     return render(request, 'wholesale.html', {})
 
 def collections(request, slug=None):
-    # Initialize variables
     collection = None
     products = None
-    collections = Collection.objects.exclude(name="CBD Packaging")  # Exclude the specific Collection
-
-     # Redirect to home if the requested collection is "CBD Packaging"
-    if slug == "cbd-packaging":  # Assuming 'slug' is in a URL-friendly format
-        return redirect('collections')  # Redirect to the home page
+    
+    # Try to get the "CBD Packaging" collection
+    cbd_packaging = get_object_or_404(Collection, name="CBD Packaging")
+    
+    # Get collections that are children of "CBD Packaging"
+    collections = Collection.objects.filter(parent=cbd_packaging)
+    
+    # Redirect to home if the requested collection slug is "cbd-packaging"
+    if slug and slug.lower() == "cbd-packaging":
+        return redirect('collections')
 
     if slug:
         try:
-            # Look up the Collection by slug
             collection = get_object_or_404(Collection, slug=slug)
-            # Get all products directly related to this Collection
             products = collection.products.filter(is_active=True)
         except Collection.DoesNotExist:
-            # Handle the case where the Collection does not exist
             messages.error(request, "That Collection Doesn't Exist.")
-            return redirect('collections')  # Redirect to the collection list if Collection doesn't exist
+            return redirect('collections')
 
     context = {
         'collection': collection,
-        'collections': collections,  # List all collections excluding "CBD PACKAGING"
-        'products': products,  # Products for a specific Collection
+        'collections': collections,
+        'products': products,
     }
 
     return render(request, 'collections.html', context)
 
 def collection_detail(request, slug):
+    # Try to get the "CBD Packaging" collection
+    cbd_packaging = get_object_or_404(Collection, name="CBD Packaging")
+    
+    # Redirect if the slug is "cbd-packaging"
+    if slug.lower() == "cbd-packaging":
+        return redirect('collections')
+    
     # Get the collection based on the slug
     collection = get_object_or_404(Collection, slug=slug)
     
     # Get all products associated with the collection
     products = Product.objects.filter(collection=collection)
     
-    # Pass the collection name (or slug) to the context
     context = {
         'collection': collection,
         'products': products,
     }
     return render(request, 'collection_detail.html', context)
+
 
 def product(request, pk):
     product = get_object_or_404(Product, id=pk)
