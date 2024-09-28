fc87ea6855c1ce5f604dab3052b2057674ae9bba
CodeLegg <michaelraymondlegg@gmail.com>
2024-09-28 11:03:54 +0100

    Refactor URL path for sticker sub-collections and CBD collections views

--- ecom/store/admin.py
+++ ecom/store/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Collection, Customer, Product, Order, ProductCollection
-from django_mptt_admin.admin import DjangoMpttAdmin
 from django.utils.text import slugify
+from mptt.admin import MPTTModelAdmin
 
 # Inline for ProductCollection to manage products in the Collection
 class ProductCollectionInline(admin.TabularInline):
@@ -10,7 +10,8 @@ class ProductCollectionInline(admin.TabularInline):
     fields = ['product', 'order']
     ordering = ['order']
 
-class CollectionAdmin(DjangoMpttAdmin):
+
+class CollectionAdmin(MPTTModelAdmin):
     list_display = ('name', 'slug', 'is_active', 'is_featured', 'order', 'parent')
     search_fields = ('name', 'description')
     prepopulated_fields = {'slug': ('name',)}
@@ -18,17 +19,22 @@ class CollectionAdmin(DjangoMpttAdmin):
 
     # Make 'order' editable directly in the list view
     list_editable = ('order',)
-    
+
+    # Use MPTT to order nodes according to the tree structure
+    mptt_level_indent = 20  # Indent children by 20 pixels in admin view
+
     # Optional: Group fields and customize the form layout
     fieldsets = (
         (None, {
-            'fields': ('name', 'slug', 'description', 'image', 'is_active', 'is_featured', 'order', 'parent'),
+            'fields': ('name', 'slug', 'description', 'image', 'is_active', 'is_featured', 'order', 'parent')
         }),
         ('Visibility Dates', {
-            'fields': ('visible_from', 'visible_to'),
+            'fields': ('visible_from', 'visible_to')
+        }),
+        ('Timestamps', {
+            'fields': ('updated_at',)
         }),
     )
-    
     readonly_fields = ('updated_at',)
 
     # Add the ProductCollection inline to manage products in the collection
@@ -40,7 +46,13 @@ class CollectionAdmin(DjangoMpttAdmin):
             obj.slug = slugify(obj.name)
         super().save_model(request, obj, form, change)
 
+    # Provide custom ordering in the list view
+    ordering = ['tree_id', 'lft']  # Tree-specific ordering to maintain hierarchy
+
+
+
 # Register your models here.
+# Register models with their respective admins
 admin.site.register(Collection, CollectionAdmin)
 admin.site.register(Customer)
 admin.site.register(Product)
