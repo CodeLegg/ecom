6427d4a8362f483a7572a76e68b31eb48c1cfd25
CodeLegg <michaelraymondlegg@gmail.com>
2024-10-02 16:23:04 +0100

    Refactor import path for 'store' app in INSTALLED_APPS

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store',
+    'ecom.store',
     'mptt',
     'django_mptt_admin',
 ]
